ce9f586aa6a4f084de8597f5c9cfa664240c5a8a
Amisha Kotadiya <90302394+AmishaKotadiya@users.noreply.github.com>
2022-01-05 10:28:29 -0800

    module-6 updated.

---
+++ module-6/WebApi/WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{    public class EmployeeController : ApiController
+    {
+        // create list type of employee object
+        public List<Employee> employees = new List<Employee>
+        {
+            new Employee(1,"amisha","kotadiya","Lalpur",DateTime.Today),
+            new Employee(2,"dhruvi","pandya","Rajkot",DateTime.Today),
+            new Employee(3,"mihir","savjani","Porbandar",DateTime.Today),
+            new Employee(4,"parth","kotadiya","Ahmedabad",DateTime.Today)
+        };
+        
+        /// <summary>
+        /// get method
+        /// </summary>
+        /// <returns>employees detail</returns>
+        [HttpGet]
+        public List<Employee> Get()
+        {
+            return employees;
+        }
+
+        /// <summary>
+        /// get method
+        /// </summary>
+        /// <param name="id">employee id</param>
+        /// <returns>employee detail</returns>
+        [HttpGet]
+        public Employee Get(int id)
+        {
+            Employee employee = employees.Find(e => e.Id == id);
+            return employee;
+        }
+
+        /// <summary>
+        /// employee post method
+        /// </summary>
+        /// <param name="employee">for employee</param>
+        /// <returns>employee detail</returns>
+        [HttpPost]
+        public List<Employee> Post([FromBody] Employee employee)
+        {
+            employees.Add(employee);
+            return employees;
+        }
+
+        /// <summary>
+        /// employee put method
+        /// </summary>
+        /// <param name="id">employee id</param>
+        /// <param name="employee">employee details</param>
+        /// <returns>employee detail</returns>
+        [HttpPut]
+        public List<Employee> Put(int id, [FromBody] Employee employee)
+        {
+            Employee friendToUpdate = employees.Find(f => f.Id == id);
+            int index = employees.IndexOf(friendToUpdate);
+
+            employees[index].Firstname = employee.Firstname;
+            employees[index].Lastname = employee.Lastname;
+            employees[index].Location = employee.Location;
+            employees[index].DateOfHire = employee.DateOfHire;
+
+            return employees;
+        }
+
+        /// <summary>
+        /// delete method
+        /// </summary>
+        /// <param name="id">employee id</param>
+        /// <returns>employee detail</returns>
+        [HttpDelete]
+        public List<Employee> Delete(int id)
+        {
+            Employee employee = employees.Find(f => f.Id == id);
+            employees.Remove(employee);
+            return employees;
+        }
+    }
+}
---
+++ module-6/WebApi/WebApi/Models/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Constuctor of employee
+        /// </summary>
+        /// <param name="v1">id</param>
+        /// <param name="v2">firstname</param>
+        /// <param name="v3">lastname</param>
+        /// <param name="v4">location</param>
+        /// <param name="today">date of hire</param>
+        public Employee(int v1, string v2, string v3, string v4, DateTime today)
+        {
+            
+        }
+
+        /// <summary>
+        /// employee id method
+        /// </summary>
+        [Required(ErrorMessage = "employee id is required")]
+        [Range(1, 150)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// employee firstname method
+        /// </summary>
+        public string Firstname { get; set; }
+
+        /// <summary>
+        /// employee lastname method
+        /// </summary>
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// employee location method
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// employee date of hiring method
+        /// </summary>
+        [Required(ErrorMessage = "employee Hire date is required")]
+        public DateTime DateOfHire { get; set; }
+    }
+}
